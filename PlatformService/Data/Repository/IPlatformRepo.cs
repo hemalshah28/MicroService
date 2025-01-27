436b58320bac37b145bd95a009409f8cb8c9415a
Hemal Shah <h.hemal12@gmail.com>
2025-01-27 09:17:41 -0500

    Added appdbcontext.

---
+++ PlatformService/Data/Repository/IPlatformRepo.cs
@@ -0,0 +1,10 @@
+namespace PlatformService.Data.Repository
+{
+    public interface IPlatformRepo
+    {
+        bool SaveChanges();
+        IEnumerable<Platform> GetAllPlatforms();
+        Platform GetPlatformById(int id);
+        void CreatePlatform(Platform platform);
+    }
+}
